93fcebfab6a86277b81cbedddc7975f17e2be70e
Oleksandr Rezanov <aleksandrrezanov17@gmail.com>
2024-02-19 21:21:25 +0200

    add utils directory

---
+++ src/utils/fetchClient.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const URL = "https://mate-academy.github.io/react_phone-catalog/api/products.json";
+
+export function getPhones() {
+  return fetch(URL).then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    }
+
+    return response.json();
+  });
+}

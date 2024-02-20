3683da825554fb9f8d7e311f85e5cc15b4eb0732
Oleksandr Rezanov <aleksandrrezanov17@gmail.com>
2024-02-20 23:45:56 +0200

    add interface Phone and SelectedPhone

--- src/types/Phone.ts
+++ src/types/Phone.ts
@@ -1,8 +1,12 @@
 export interface Phone {
-  id: number;
+  age: number;
+  id: string;
+  type: string;
+  imageUrl: string;
   name: string;
-  price: string;
-  image: string;
+  snippet: string;
+  price: number;
+  discount: number;
   screen: string;
   capacity: string;
   ram: string;

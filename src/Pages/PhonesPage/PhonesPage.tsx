6f90f6791b98383b517439a4744bcd3401c10f8d
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-02-24 18:20:58 +0200

    added loader

--- src/Pages/PhonesPage/PhonesPage.tsx
+++ src/Pages/PhonesPage/PhonesPage.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
 import React, { useEffect, useState } from "react";
 import "./PhonesPage.scss";

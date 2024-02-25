25ee1fcb3d8dca3b10802acae5007ebca2be1ff5
avakiel <79792776+avakiel@users.noreply.github.com>
2024-02-25 19:15:05 +0200

    fix card layout scss

--- src/Pages/PhonesPage/PhonesPage.tsx
+++ src/Pages/PhonesPage/PhonesPage.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable prettier/prettier */
--- src/utils/paginate.ts
+++
@@ -1,8 +0,0 @@
-import { Phone } from "../types/Phone";
-
-export function paginateArray(array: Phone[], currentPage: number, itemsOnPage: number) {
-  const startIndex = currentPage * itemsOnPage;
-  const endIndex = startIndex + itemsOnPage;
-
-  return array.slice(startIndex, endIndex);
-}

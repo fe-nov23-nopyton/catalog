f1ec490ae8c72575ddaa999688dab2f59181a66b
Nazarii Saviak <151225766+ssaviak@users.noreply.github.com>
2024-02-26 19:04:24 +0200

    Create ButtonsHeader.tsx

---
+++ src/header/ButtonsHeader/ButtonsHeader.tsx
@@ -0,0 +1,25 @@
+import "./buttonsHeader.scss";
+import { useAppSelector } from "../../redux/hooks";
+import IconCount from "../../Components/IconCount/IconCount";
+import { NavLink } from "react-router-dom";
+import { navLinkClassName } from "../../utils/navLinkClassName";
+
+export const ButtonsHeader = () => {
+  const { cart } = useAppSelector((state) => state.cart);
+  const { favorites } = useAppSelector((state) => state.favorites);
+
+  return (
+    <>
+      <NavLink className={navLinkClassName} to={"/catalog/favorites"}>
+        <div className="icon-favorite">
+          <IconCount items={favorites} />
+        </div>
+      </NavLink>
+      <NavLink className={navLinkClassName} to={"/catalog/cart"}>
+        <div className="icon-cart">
+          <IconCount items={cart} />
+        </div>
+      </NavLink>
+    </>
+  );
+};

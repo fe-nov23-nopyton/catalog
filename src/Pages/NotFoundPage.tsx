8402eb0b17ded133e5cd79cfb4aae721b274cace
Maksym Kalashnyk <maxplay123890@gmail.com>
2024-02-27 20:42:16 +0200

    add not found

--- src/Pages/NotFoundPage.tsx
+++ src/Pages/NotFoundPage.tsx
@@ -1,3 +1,17 @@
 import React from "react";
 
-export const NotFoundPage: React.FC = () => <h1 className="title">Page not found</h1>;
+import "../Pages/NotFoundPage.scss";
+
+export const NotFoundPage: React.FC = () => (
+  <div className="not-found">
+    <div className="looking-guy forward">
+      <div className="eye left-eye" />
+      <div className="eye right-eye" />
+    </div>
+    <h1 className="not-found__title">404 - Page not found</h1>
+    <p className="not-found__message">Oops! It seems like you got lost...</p>
+    <a href="/catalog/home" className="not-found__home-link">
+      Go back to Home
+    </a>
+  </div>
+);

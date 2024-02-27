6f63aeefbe6e5edd1734c118f6b0f88d5b8810e8
Vadym Sainiuk <vadik.sajnuykk@gmail.com>
2024-02-28 01:09:13 +0200

    rewrited contacts component

---
+++ src/Pages/Contacts/Person/Person.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Person.scss";
+
+interface Props {
+  photo: string;
+  name: string;
+  phone: string;
+  email: string;
+}
+
+export const Person: React.FC<Props> = ({ phone, name, email, photo }) => {
+  console.log("Person page");
+  return (
+    <div className="person">
+      <div className="person__image-container">
+        <img className="person__image" src={photo} alt={name} />
+      </div>
+
+      <div className="person__details">
+        <h2 className="person__details-name">{name}</h2>
+
+        <div className="person__feature">
+          <p className="person__feature-name">email: </p>
+
+          <p className="person__feature-value">{email}</p>
+        </div>
+        <div className="person__feature">
+          <p className="person__feature-name">phone:</p>
+
+          <p className="person__feature-value">{phone}</p>
+        </div>
+      </div>
+    </div>
+  );
+};

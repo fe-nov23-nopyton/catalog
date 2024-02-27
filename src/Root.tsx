3829de01f1196418391e40c739045c06847313d3
ssaviak <saviaknazarii.dev@gmail.com>
2024-02-27 23:20:50 +0200

    hash

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { App } from "./App";
 import { HomePage } from "./Pages/HomePage";
 import { NotFoundPage } from "./Pages/NotFoundPage/NotFoundPage";

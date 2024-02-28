5c5a32767a6fe8b87f989ce7ecba7985268d2312
Maksym Kalashnyk <maxplay123890@gmail.com>
2024-02-28 14:47:00 +0200

    add new version of modal window

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Modal.scss";
 import { useAppDispatch } from "../../redux/hooks";
 import { deleteCartItems } from "../../redux/features/cartSlice";
@@ -6,6 +7,7 @@ import { ThemeContext } from "../../ThemeContext/ThemeContext";
 
 export const Modal = () => {
   const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   const [active, setActive] = useState(false);
   const { theme } = useContext(ThemeContext);
 
@@ -13,23 +15,26 @@ export const Modal = () => {
     setActive(true);
     const timerId = setTimeout(() => {
       dispatch(deleteCartItems());
-      window.location.href = "/catalog/home";
+      navigate("/catalog/home");
     }, 3000);
 
     return () => clearTimeout(timerId);
   }, []);
 
   return (
-    <div
-      className={`modal ${active ? "active" : ""} ${theme === "dark-theme" ? "dark-theme" : ""}`}
-      onClick={() => setActive(false)}
-    >
-      <div className={`modal__content ${active ? "active" : ""}`}>
-        <div className="modal__image" />
-        <h2 className="modal__title">Congrats!</h2>
-        <h3 className="modal__thanks">Thank you for your purchase!</h3>
-        <p className="modal__subtitle">We appreciate your purchase. Enjoy your products!</p>
+    <div>
+      <div
+        className={`modal ${active ? "active" : ""} ${theme === "dark-theme" ? "dark-theme" : ""}`}
+        onClick={() => setActive(false)}
+      >
+        <div className={`modal__content ${active ? "active" : ""}`}>
+          <div className="modal__image" />
+          <h2 className="modal__title">Congrats!</h2>
+          <h3 className="modal__thanks">Thank you for your purchase!</h3>
+          <p className="modal__subtitle">We appreciate your purchase. Enjoy your products!</p>
+        </div>
       </div>
+      {active && <div className="modal-overlay" />}
     </div>
   );
 };
